9cc6e8aa3f2ff1b28ab86b08368505f9e6e78943
Tuan Dat <phamtuandat.work@gmail.com>
2025-12-30 21:56:59 +0700

    Switch database from dj_database_url to SQLite
    Changed database configuration from dj_database_url to SQLite.

--- diary_app/settings.py
+++ diary_app/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 from pathlib import Path
-import dj_database_url
+# import dj_database_url
 
 
 
@@ -96,10 +96,12 @@ WSGI_APPLICATION = "diary_app.wsgi.application"
 #     }
 # }
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 
 
