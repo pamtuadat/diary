fc0b6939da225b485af9bb5bef7124d20013aa25
Tuan Dat <phamtuandat.work@gmail.com>
2025-12-31 19:35:18 +0700

    Update settings.py

--- diary_app/settings.py
+++ diary_app/settings.py
@@ -85,22 +85,22 @@ WSGI_APPLICATION = "diary_app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "diary_db",
-#         "USER": 'postgres',
-#         "PASSWORD":'123456',
-#         "HOST":'localhost',
-#         "PORT":"5432"
-#     }
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "diary_db",
+        "USER": 'postgres',
+        "PASSWORD":'123456',
+        "HOST":'localhost',
+        "PORT":"5432"
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
